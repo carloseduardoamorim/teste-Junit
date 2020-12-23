331f00aa591303f22cef2158b203b3261d48e662
carloseduardoamorim <caduamorim13@gmail.com>
2020-12-23 11:01:27 -0300

    Comitando o primeiro teste no Junit

---
+++ src/test/java/com/mycompany/introducaojunit5/MathUtiilTest.java
@@ -0,0 +1,26 @@
+
+package com.mycompany.introducaojunit5;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathUtiilTest {
+
+    @Test
+    void testMdcAParP1() {
+        final int a = 6;
+        final int b = 3;
+        final int esperado = b;
+        final int obtido = MathUtiil.mdc(a, b);
+        assertEquals(esperado, obtido);
+    }
+    
+    @Test
+    void testMdcAImparP1(){
+        final int a = 9;
+        final int b = 3;
+        final int esperado = b;
+        final int obtido = MathUtiil.mdc(a, b);
+        assertEquals(esperado, obtido);
+    }
+}
