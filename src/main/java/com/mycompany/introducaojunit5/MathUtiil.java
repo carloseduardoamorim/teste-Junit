7b89e7f73d7fab2cdd7ead2ddbf5b3e8ff181880
carloseduardoamorim <caduamorim13@gmail.com>
2020-12-30 00:10:59 -0300

    #4 aplicando TDD - MDC

--- src/main/java/com/mycompany/introducaojunit5/MathUtiil.java
+++ src/main/java/com/mycompany/introducaojunit5/MathUtiil.java
@@ -3,9 +3,29 @@ package com.mycompany.introducaojunit5;
 
 public class MathUtiil {
     public static int mdc(int a, int b) {
+        a = Math.abs(a);
+        b = Math.abs(b);
+        
+        // Propriendade 6
+        final int maior = Math.max(a, b);
+        b = Math.min(a, b);
+        a = maior;
+        
+        // Propriedade 1
         if (b > 0 && a % b == 0){
             return b;
         }
-        return 0;
+        
+        // Propriendade 3
+        if(b == 0){
+            return Math.abs(a);
+        }
+        
+        // Propriendade 5
+        if(a % b != 0){
+            return 1;
+        }
+        
+        return -1;
     }
 }
